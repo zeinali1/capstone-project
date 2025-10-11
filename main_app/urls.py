c38beac7fd58b3de8b2f52598fc2823cc522de31
zeinali1 <zein.ali1@yahoo.com>
2025-10-11 19:06:32 +0300

    create events folder with their corresponding views and urls.py updates

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from . import views
 
 
 urlpatterns = [
-   
+   path('', views.HomeView.as_view(), name='home'),
+   path('auth/signup/',views.SignUpView.as_view(), name='signup'),
+   path('event/<int:pk>/', views.EventDetailView.as_view(), name='event_detail'),
+   path('event/new/', views.EventCreateView.as_view(), name='create_event'),
+   path('event/<int:pk>/edit/', views.EventUpdateView.as_view(), name='edit_event'),
+   path('event/<int:pk>/delete/', views.EventDeleteView.as_view(), name='delete_event'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,75 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from django.shortcuts import redirect, get_object_or_404
+from django.utils import timezone
+from django.contrib.auth import login
+from django.views import View
+
+from .models import Event, Registration
+from .forms import EventForm, RegisterForm
 
 # Create your views here.
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['events'] = Event.objects.filter(event_date__gte=timezone.now()).order_by('event_date')
+        context['past_events'] = Event.objects.filter(event_date__lt=timezone.now()).order_by('-event_date')[:5]
+        return context
+    
+class SignUpView(CreateView):
+    form_class = RegisterForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return response
+    
+class EventDetailView(DetailView):
+    model = Event
+    template_name = 'events/event_detail.html'
+    context_object_name = 'event'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        event = self.get_object()
+        user = self.request.user
+        context['is_joined'] = user.is_authenticated and Registration.objects.filter(user=user, event=event).exists()
+        return context
+    
+class EventCreateView(LoginRequiredMixin, CreateView):
+    model = Event
+    form_class = EventForm
+    template_name = 'events/event_form.html'
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+    
+class EventUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Event
+    form_class = EventForm
+    template_name = 'events/event_form.html'
+
+    def test_func(self):
+        event = self.get_object()
+        return self.request.user == event.created_by
+
+    def get_success_url(self):
+        return reverse_lazy('event_detail', kwargs={'pk': self.object.pk})
+
+class EventDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Event
+    template_name = 'events/event_confirm_delete.html'
+    success_url = reverse_lazy('home')
+
+    def test_func(self):
+        event = self.get_object()
+        return self.request.user == event.created_by

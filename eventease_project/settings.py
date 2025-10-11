d974894223e61fea3a451c57d61ff3e6dab6d50f
zeinali1 <zein.ali1@yahoo.com>
2025-10-11 19:02:37 +0300

    create registration folder, base.html, and home.html

--- eventease_project/settings.py
+++ eventease_project/settings.py
@@ -126,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Auth Redirects
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "homepage"       # after successful login
+LOGOUT_REDIRECT_URL = "login"     # after logout
